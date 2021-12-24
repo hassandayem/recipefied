2cb3f3349782a1b73e7a0cc65b39fc8e824ecb93
hassandayem <hassandayem91@gmail.com>
2021-12-24 07:06:03 +0300

    added ThemProvider context to control the nav color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 // import the style
 import "./Navbar.css";
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Recepified</h1>
